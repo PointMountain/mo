e1210fcc9f42c76967b9458a183a0ab6d94aee84
lucas.jing <lucas.jing@drigle.com>
2020-04-28 11:09:51 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,10 @@
 module.exports = {
   title: '个人文档',
-  description: '练习文档'
+  description: '练习文档',
+  themeConfig: {
+    nav: [
+      {text: 'Home', link: '/'},
+      {text: 'Guide', link: '/guide/'}
+    ]
+  }
 }
