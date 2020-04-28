c32bf0cee03585586b06eb8d490f372d9ec28880
lucas.jing <lucas.jing@drigle.com>
2020-04-28 15:33:44 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
   base: '/mo/',
   title: '个人文档',
   description: '练习文档',
+  head: [
+    ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `/favicon.ico` }]
+  ],
   themeConfig: {
     nav: [
       {text: 'Home', link: '/'},
